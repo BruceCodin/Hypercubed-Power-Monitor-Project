2fe71d6943c51f4aa948a05e821f25ba3a9d8f60
jasmine-cornelius <jasmineliz.cornelius@gmail.com>
2025-11-18 17:50:24 +0000

    feature/terraform/secrets: initial draft of terraform secrets

---
+++ terraform/terraform_secrets/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region     = var.aws_region
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name        = "power-monitor-db-credentials"
+  description = "Database credentials for power monitor RDS"
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    DB_HOST     = var.db_host
+    DB_PORT     = var.db_port
+    DB_NAME     = var.db_name
+    DB_USER     = var.db_user
+    DB_PASSWORD = var.db_password
+    DB_SCHEMA   = var.db_schema
+  })
+}
---
+++ terraform/terraform_secrets/variables.tf
@@ -0,0 +1,42 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default = "eu-west-2"
+}
+
+
+variable "db_host" {
+  description = "Database host endpoint"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_port" {
+  description = "Database port"
+  type        = number
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_user" {
+  description = "Database username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_schema" {
+  description = "Database schema"
+  type        = string
+  sensitive   = true
+}
