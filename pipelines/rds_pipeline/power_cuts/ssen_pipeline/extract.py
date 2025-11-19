d1e6784eda5d6a30cc58b64907bbbe7276a71b84
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-19 13:46:56 +0000

    Revert "feat: high-level function that runs full extract script. Add logging. Removed 'region' when parsing entries"
    This reverts commit 42d9b136d864e730c002d3c408d8261ef46d5219.

--- pipelines/rds_pipeline/power_cuts/ssen_pipeline/extract.py
+++ pipelines/rds_pipeline/power_cuts/ssen_pipeline/extract.py
@@ -10,15 +10,15 @@ from pprint import pprint
 BASE_URL = "https://ssen-powertrack-api.opcld.com/gridiview/reporter/info/livefaults"
 PROVIDER = "Scottish and Southern Electricity Networks"
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 logger = logging.getLogger(__name__)
 
 
 def extract_power_cut_data() -> Optional[dict]:
     """
     Fetch raw data from SSEN power cut API.
+
+    Args:
+        url (str): The API endpoint URL to fetch power cut data from.
 
     Returns:
         dict: The raw data fetched from the SSEN power cut API.
@@ -54,37 +54,24 @@ def parse_power_cut_data(data: dict) -> list[dict]:
         return parsed_data
 
     for fault in data['Faults']:
-
         parsed_entry = {
             "source_provider": PROVIDER,
             "status": fault.get("py/object"),
+            "region_affected": fault.get("name"),
             "outage_date": fault.get("loggedAt"),
             "recording_time": datetime.now().isoformat(),
             "affected_postcodes": fault.get("affectedAreas"),
         }
-
         parsed_data.append(parsed_entry)
 
     logger.info("Data parsing successful.")
     return parsed_data
 
 
-def get_power_cut_data() -> list[dict]:
-    """
-    High-level function to get parsed power cut data from SSEN API.
-
-    Returns:
-        list[dict]: A list of dictionaries containing parsed power cut information.
-    """
-
-    raw_data = extract_power_cut_data()
-    if raw_data:
-        return parse_power_cut_data(raw_data)
-    else:
-        return []
-
-
 if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     data = extract_power_cut_data()
     # pprint(data)
