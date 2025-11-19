31d1a460b704fc2e0ce5833271ffbd55acbffbe1
jasmine-cornelius <jasmineliz.cornelius@gmail.com>
2025-11-19 11:09:17 +0000

    Remove DB_SCHEMA from main.tf
    Remove DB_SCHEMA variable from database configuration.

--- terraform/terraform_secrets/main.tf
+++ terraform/terraform_secrets/main.tf
@@ -15,6 +15,5 @@ resource "aws_secretsmanager_secret_version" "db_credentials" {
     DB_NAME     = var.db_name
     DB_USER     = var.db_user
     DB_PASSWORD = var.db_password
-    DB_SCHEMA   = var.db_schema
   })
 }
