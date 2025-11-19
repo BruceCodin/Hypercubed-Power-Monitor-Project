32dbbd5d34e65857a07335cf0d0a0e386546f4e4
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-19 16:59:26 +0000

    feat: Add extract script for Norther Powergrid

---
+++ pipelines/rds_pipeline/power_cuts/northern_powergrid_pipeline/extract.py
@@ -0,0 +1,84 @@
+# pylint: disable=redefined-outer-name
+
+"""Module to extract power cut data from Northern Powergrid API.
+Seems to update every ~30 minutes."""
+
+from datetime import datetime
+import logging
+from typing import Optional
+from pprint import pprint
+import requests as req
+
+BASE_URL = "https://power.northernpowergrid.com/Powercut_API/rest/powercuts/getall"
+PROVIDER = "Northern Powergrid"
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def extract_power_cut_data() -> Optional[dict]:
+    """
+    Fetch raw data from Northern Powergrid power cut API.
+
+    Returns:
+        dict: The raw data fetched from the Northern Powergrid power cut API.
+    """
+
+    try:
+        response = req.get(BASE_URL, timeout=10)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
+        logger.info("Data extraction successful.")
+        return data
+
+    except req.exceptions.RequestException as e:
+        logger.error("API request failed: %s", e)
+        return None
+
+
+def parse_power_cut_data(data: dict) -> list[dict]:
+    """
+    Parse the raw data from Northern Powergrid power cut API to extract relevant information.
+
+    Args:
+        data (dict): The raw data fetched from the Northern Powergrid power cut API.
+
+    Returns:
+        list[dict]: A list of dictionaries containing parsed power cut information.
+    """
+
+    parsed_data = []
+
+    if not data:
+        logger.warning("No valid data to parse.")
+        return parsed_data
+
+    for fault in data:
+        pprint(fault)
+        print("-----")
+        parsed_data.append({
+            "source_provider": PROVIDER,
+            "status": fault.get("NatureOfOutage"),
+            "outage_date": fault.get("LoggedTime"),
+            "recording_time": datetime.now().isoformat(),
+            "affected_postcodes": fault.get("Postcode")
+        })
+
+    logger.info("Data parsing successful.")
+    return parsed_data
+
+
+if __name__ == "__main__":
+
+    # Example usage for local testing
+
+    data = extract_power_cut_data()
+
+    if data:
+        parsed_data = parse_power_cut_data(data)
+        print("Extracted and Parsed Data:")
+        for entry in parsed_data:
+            pprint(entry)
+            print("-----")
+        print(f"Total records extracted: {len(parsed_data)}")
