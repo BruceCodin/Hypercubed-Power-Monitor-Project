a259ff763505c1d3c1b4bb4993c7a8ae1229367b
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-19 15:55:17 +0000

    feat: Add extract script for SP North West provider

---
+++ pipelines/rds_pipeline/power_cuts/sp_northwest_pipeline/extract.py
@@ -0,0 +1,79 @@
+# pylint: disable=redefined-outer-name
+
+"""Module to extract power cut data from North West Electricity (SP Energy Networks) API.
+Seems to update ~ every 5 minutes."""
+
+from datetime import datetime
+import logging
+from typing import Optional
+from pprint import pprint
+import requests as req
+
+BASE_URL = "https://www.enwl.co.uk/api/power-outages/search?pageSize=1000&pageNumber=1&includeCurrent=true&includeResolved=false&includeTodaysPlanned=true&includeFuturePlanned=true&includeCancelledPlanned=false"
+PROVIDER = "SP Electricity North West"
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def extract_power_cut_data() -> Optional[dict]:
+    """
+    Fetch raw data from SP Electricity North West power cut API.
+
+    Returns:
+        dict: The raw data fetched from the SP Electricity North West power cut API.
+    """
+
+    try:
+        response = req.get(BASE_URL, timeout=10)
+        response.raise_for_status()  # Raise an error for bad responses
+        data = response.json()
+        logger.info("Data extraction successful.")
+        return data
+
+    except req.exceptions.RequestException as e:
+        logger.error("API request failed: %s", e)
+        return None
+
+
+def parse_power_cut_data(data: dict) -> list[dict]:
+    """
+    Parse the raw data from SP Electricity North West power cut API to extract relevant information.
+
+    Args:
+        data (dict): The raw data fetched from the SP Electricity North West power cut API.
+
+    Returns:
+        list[dict]: A list of dictionaries containing parsed power cut information.
+    """
+
+    parsed_data = []
+
+    if not data or 'Items' not in data:
+        logger.warning("No valid data to parse.")
+        return parsed_data
+
+    for fault in data['Items']:
+        parsed_data.append({
+            "source_provider": PROVIDER,
+            "status": fault.get("Type"),
+            "outage_date": fault.get("date"),
+            "recording_time": datetime.now().isoformat(),
+            "affected_postcodes": fault.get("AffectedPostcodes")
+        })
+
+    logger.info("Data parsing successful.")
+    return parsed_data
+
+
+if __name__ == "__main__":
+
+    # Example usage for local testing
+
+    data = extract_power_cut_data()
+
+    if data:
+        parsed_data = parse_power_cut_data(data)
+        print("Extracted and Parsed Data:")
+        pprint(parsed_data)
