d850be31c5571519fe2f74a5d972d43af2a4896f
jasmine-cornelius <jasmineliz.cornelius@gmail.com>
2025-11-20 09:43:05 +0000

    Add outputs for S3 bucket name and ARN
    Added outputs for bucket name and ARN.

--- terraform/terraform_s3/main.tf
+++ terraform/terraform_s3/main.tf
@@ -5,3 +5,13 @@ provider "aws" {
 resource "aws_s3_bucket" "data_bucket" {
   bucket = var.s3_bucket_name
 }
+
+output "bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.data_bucket.id
+}
+
+output "bucket_arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.data_bucket.arn
+}
