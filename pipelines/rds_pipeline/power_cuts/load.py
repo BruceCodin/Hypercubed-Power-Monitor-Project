837933025a660f25bfbf98af6fa9a448a43e6465
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-21 10:34:54 +0000

    feat: Main load function to pull from all pipelines and load data to RDS

---
+++ pipelines/rds_pipeline/power_cuts/load.py
@@ -0,0 +1,96 @@
+import psycopg2
+import logging
+import os
+from dotenv import load_dotenv
+
+###
+#
+# Import extraction and transformation functions from each pipeline
+#
+###
+
+load_dotenv()
+
+
+def connect_to_database():
+    """Connects to your AWS Postgres database and ensures tables exist."""
+
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+
+    return conn
+
+
+def insert_data(conn, data):
+
+    cursor = conn.cursor()
+    number_inserted = 0
+
+    try:
+        for entry in data:
+            # 1. Insert into Parent Table (FACT_outage)
+            # Using ON CONFLICT to handle duplicates based on the unique index
+            cursor.execute('''
+                INSERT INTO FACT_outage (source_provider, outage_date, recording_time, status)
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT (source_provider, outage_date) DO NOTHING
+                RETURNING outage_id
+            ''', (
+                entry['source_provider'],
+                entry['outage_date'],
+                entry['recording_time'],
+                entry['status']
+            ))
+
+            # 2. Check if we actually inserted anything
+            result = cursor.fetchone()
+
+            if result:
+                new_outage_id = result[0]
+
+                # 3. Insert into the Child/Bridge Table (BRIDGE_affected_postcodes)
+                postcodes = entry['affected_postcodes']
+
+                bridge_data = [(new_outage_id, code) for code in postcodes]
+
+                cursor.executemany('''
+                    INSERT INTO BRIDGE_affected_postcodes (outage_id, postcode_affected)
+                    VALUES (%s, %s)
+                ''', bridge_data)
+
+                number_inserted += 1
+
+            else:
+                pass
+
+        conn.commit()
+        logging.info(
+            "Inserted %d records into FACT_outage and BRIDGE_affected_postcodes.", number_inserted)
+
+        logging.info("Skipped %d duplicate records.",
+                     len(data) - number_inserted)
+
+    except (psycopg2.Error, Exception) as e:
+        logging.error(f"An error occurred: {e}")
+        conn.rollback()
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s | %(levelname)s | %(filename)s | %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    raw_data = extract_power_cut_data()
+    transformed_data = transform_power_cut_data(raw_data)
+
+    db_conn = connect_to_database()
+
+    insert_data(db_conn, transformed_data)
+
+    db_conn.close()
