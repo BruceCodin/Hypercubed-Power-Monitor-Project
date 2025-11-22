f1eeac2d811d1b98cf08052d2bb0288261ccd840
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-22 13:05:27 +0000

    feat: Add lambda_handler that orchestrates ETL for all pipelines. Passes local testing. Add requirements.txt containing libraries used across all pipelines.

---
+++ pipelines/rds_pipeline/power_cuts/lambda_handler_power_cuts.py
@@ -0,0 +1,171 @@
+"""AWS Lambda handler for power cuts data pipelines."""
+
+import logging
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+
+from national_grid_pipeline.extract_national_grid import extract_data_national_grid
+from national_grid_pipeline.transform_national_grid import transform_data_national_grid
+
+from nie_networks_pipeline.extract_nie import extract_nie_data
+from nie_networks_pipeline.transform_nie import transform_nie_data
+
+from northern_powergrid_pipeline.extract_northern_powergrid import extract_northern_powergrid_data
+from northern_powergrid_pipeline.transform_northern_powergrid import transform_northern_powergrid_data
+
+from sp_energy_pipeline.extract_sp_en import extract_data_sp_en
+from sp_energy_pipeline.transform_sp_en import transform_data_sp_en
+
+from sp_northwest_pipeline.extract_sp_northwest import extract_data_sp_northwest
+from sp_northwest_pipeline.transform_sp_northwest import transform_data_sp_northwest
+
+from ssen_pipeline.extract_ssen import extract_ssen_data
+from ssen_pipeline.transform_ssen import transform_ssen_data
+
+from uk_power_networks_pipeline.extract_uk_pow import extract_data_uk_pow
+from uk_power_networks_pipeline.transform_uk_pow import transform_data_uk_pow
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s - %(filename)s', level=logging.INFO)
+logger = logging.getLogger()
+
+
+def connect_to_database() -> psycopg2.extensions.connection:
+    """Connects to AWS Postgres database using Secrets Manager credentials.
+
+    Returns:
+        psycopg2 connection object
+    """
+
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USERNAME"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
+
+    return conn
+
+
+def insert_data(conn, data):
+    """Insert power cut data into the database.
+
+    Args:
+        conn: psycopg2 connection object
+        data: List of dictionaries containing power cut data
+    """
+
+    cursor = conn.cursor()
+    number_inserted = 0
+
+    for entry in data:
+        cursor.execute('''
+            INSERT INTO FACT_outage (source_provider, outage_date, recording_time, status)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT (source_provider, outage_date) DO NOTHING
+            RETURNING outage_id
+        ''', (
+            entry['source_provider'],
+            entry['outage_date'],
+            entry['recording_time'],
+            entry['status']
+        ))
+
+        result = cursor.fetchone()
+
+        if result:
+            new_outage_id = result[0]
+            postcodes = entry['affected_postcodes']
+            bridge_data = [(new_outage_id, code) for code in postcodes]
+
+            cursor.executemany('''
+                INSERT INTO BRIDGE_affected_postcodes (outage_id, postcode_affected)
+                VALUES (%s, %s)
+            ''', bridge_data)
+
+            number_inserted += 1
+
+    conn.commit()
+    return number_inserted
+
+
+def lambda_handler(event, context):
+    """AWS Lambda handler function for power cuts ETL pipelines."""
+
+    load_dotenv()
+
+    db_conn = connect_to_database()
+
+    # National Grid
+    raw_data = extract_data_national_grid()
+    if raw_data:
+        transformed_data = transform_data_national_grid(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from National Grid")
+
+    # NIE Networks
+    raw_data = extract_nie_data()
+    if raw_data:
+        transformed_data = transform_nie_data(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from NIE Networks")
+
+    # Northern Powergrid
+    raw_data = extract_northern_powergrid_data()
+    if raw_data:
+        transformed_data = transform_northern_powergrid_data(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from Northern Powergrid")
+
+    # SP Energy Networks
+    raw_data = extract_data_sp_en()
+    if raw_data:
+        transformed_data = transform_data_sp_en(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from SP Energy Networks")
+
+    # SP Northwest
+    raw_data = extract_data_sp_northwest()
+    if raw_data:
+        transformed_data = transform_data_sp_northwest(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from SP Northwest")
+
+    # SSEN
+    raw_data = extract_ssen_data()
+    if raw_data:
+        transformed_data = transform_ssen_data(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from SSEN")
+
+    # UK Power Networks
+    raw_data = extract_data_uk_pow()
+    if raw_data:
+        transformed_data = transform_data_uk_pow(raw_data)
+        insert_data(db_conn, transformed_data)
+    else:
+        logger.warning("No data from UK Power Networks")
+
+    db_conn.close()
+
+    return {
+        'statusCode': 200,
+        'body': 'Power cuts ETL execution completed'
+    }
+
+
+if __name__ == "__main__":
+
+    # For local testing of the lambda handler
+    response = lambda_handler(None, None)
+    print(response)
