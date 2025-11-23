0363b127efa48b495372c37458700b5731f6c006
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-23 15:35:02 +0000

    feat: Add bash script to update the alerts lambda image

--- pipelines/alerts_pipeline/bash_scripts/update_alerts_lambda_image.sh
+++ pipelines/alerts_pipeline/bash_scripts/update_alerts_lambda_image.sh
@@ -1,3 +1,3 @@
 aws lambda update-function-code \
-    --function-name c20-power-monitor-powercuts-etl-lambda \
-    --image-uri 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-power-monitor-powercuts-etl-repo:latest
+    --function-name c20-power-monitor-lambda-alerts-pipeline  \
+    --image-uri 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-power-monitor-alerts-etl-repo:latest
