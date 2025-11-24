e5c5e375ab5fb9397855477e242573f4569c2dd7
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-24 12:06:45 +0000

    feat: lambda orchestrates entire ETL process for uploading power cut data to s3. Add logging

--- pipelines/s3_pipeline/rds_to_s3_lambda.py
+++ pipelines/s3_pipeline/rds_to_s3_lambda.py
@@ -1,10 +1,36 @@
+from extract_from_rds import get_historical_power_cut_data
+from load_to_s3 import upload_data_to_s3
+
+import logging
+
+# Configure logging for Lambda
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 def lambda_handler(event, context):
     """Main Lambda function handler."""
 
-    # Load secrets from AWS Secrets Manager
-    secrets = get_secrets()
-    load_secrets_to_env(secrets)
+    # Fetch historical power cut data
+    logger.info("Fetching historical power cut data from RDS...")
+    data = get_historical_power_cut_data()
+    logger.info("Data fetched successfully.")
 
-    # Connect to the database
-    conn = connect_to_database()
-    cursor = conn.cursor()
+    # Upload data to S3
+    logger.info("Uploading data to S3...")
+    upload_data_to_s3(data)
+    logger.info("Data uploaded to S3 successfully.")
+
+    return {
+        'statusCode': 200,
+        'body': 'Power cut data successfully extracted from RDS and uploaded to S3.'
+    }
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    response = lambda_handler(None, None)
+    print(response)
