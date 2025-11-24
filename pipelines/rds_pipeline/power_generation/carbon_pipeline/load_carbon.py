7c57ccd4b2b936d6cb50481428a6b67db63d78cf
Charlie <trainee.charlie.alston@sigmalabs.co.uk>
2025-11-24 11:30:56 +0000

    feat: added rds credentials

--- pipelines/rds_pipeline/power_generation/carbon_pipeline/load_carbon.py
+++ pipelines/rds_pipeline/power_generation/carbon_pipeline/load_carbon.py
@@ -1,28 +1,62 @@
 '''Load carbon generation data to the RDS database.'''
 import logging
 import os
+import boto3
+import json
 import psycopg2
 from psycopg2.extras import execute_values
 #pylint: disable=logging-fstring-interpolation
 
 
-logger = logging.getLogger(__name__)
-def get_db_connection():
-    '''Establish a connection to the RDS database.'''
-    try:
-        logger.info("Attempting to connect to the database")
-        # Will change to RDS credentials later
-        connection = psycopg2.connect(
-            host="localhost",
-            database="postgres",
-            user="charliealston"
-        )
-        logger.info("Successfully connected to the database")
-        return connection
-    except psycopg2.OperationalError as e:
-        logger.error(f"Operational error connecting to database: {e}")
-        return None
+SECRETS_ARN = "arn:aws:secretsmanager:eu-west-2:129033205317:secret:c20-power-monitor-db-credentials-TAc5Xx"
 
+logger = logging.getLogger(__name__)
+def get_secrets() -> dict:
+    """Retrieve database credentials from AWS Secrets Manager.
+
+    Returns:
+        dict: Dictionary containing database credentials
+    """
+
+    client = boto3.client('secretsmanager')
+
+    response = client.get_secret_value(
+        SecretId=SECRETS_ARN
+    )
+
+    # Decrypts secret using the associated KMS key.
+    secret = response['SecretString']
+    secret_dict = json.loads(secret)
+
+    return secret_dict
+
+
+def load_secrets_to_env(secrets: dict):
+    """Load database credentials from Secrets Manager into environment variables.
+
+    Args:
+        secrets (dict): Dictionary containing database credentials"""
+
+    for key, value in secrets.items():
+        os.environ[key] = str(value)
+
+
+def connect_to_database() -> psycopg2.extensions.connection:
+    """Connects to AWS Postgres database using Secrets Manager credentials.
+
+    Returns:
+        psycopg2 connection object
+    """
+
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=int(os.getenv("DB_PORT")),
+    )
+
+    return conn
 def load_settlement_data_to_db(connection, settlement_df):
     '''
     Load the settlement data into the RDS database and returns settlement_ids.
@@ -143,6 +177,8 @@ if __name__ == "__main__":
     )
     tranform_data = transform_carbon_data(raw_data)
     print(tranform_data.head())
-    db_connection = get_db_connection()
+    secrets= get_secrets()
+    load_secrets_to_env(secrets)
+    db_connection = connect_to_database()
     # uploads carbon data to rds
     load_carbon_data_to_db(db_connection, tranform_data)
