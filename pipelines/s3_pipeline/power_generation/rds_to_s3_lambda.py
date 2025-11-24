eb9c652bee3015cd20dd56cde731d207d00398e8
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-24 19:50:44 +0000

    feat: Add main lambda to orchestrate RDS to S3 pipeline for power generation

--- pipelines/s3_pipeline/power_generation/rds_to_s3_lambda.py
+++ pipelines/s3_pipeline/power_generation/rds_to_s3_lambda.py
@@ -2,8 +2,15 @@
 
 import logging
 
-from extract_from_rds import get_historical_power_cut_data
-from load_to_s3 import upload_data_to_s3
+from extract_from_rds import (get_secrets,
+                              load_secrets_to_env,
+                              connect_to_database,
+                              get_historical_power_generation_data,
+                              get_historical_carbon_data)
+from load_to_s3 import (prepare_carbon_data,
+                        prepare_power_cut_data,
+                        upload_carbon_data_to_s3,
+                        upload_energy_generation_data_to_s3)
 
 
 # Configure logging for Lambda
@@ -15,14 +22,34 @@ def lambda_handler(event, context):
     """Main Lambda function handler."""
 
     try:
-        # Fetch historical power cut data
-        logger.info("--- START FETCHING DATA FROM RDS ---")
-        data = get_historical_power_cut_data()
-        logger.info("--- DATA FETCHED SUCCESSFULLY FROM RDS ---")
-        # Upload data to S3
-        logger.info("--- START UPLOADING DATA TO S3 ---")
-        upload_data_to_s3(data)
-        logger.info("--- DATA UPLOADED SUCCESSFULLY TO S3 ---")
+        logger.info("Loading secrets from Secrets Manager...")
+        secrets = get_secrets()
+        logger.info("Secrets loaded successfully.")
+
+        logger.info("Loading secrets into environment variables...")
+        load_secrets_to_env(secrets)
+        logger.info("Secrets loaded into environment variables.")
+
+        logger.info("Establishing database connection...")
+        conn = connect_to_database()
+        logger.info("Database connection established.")
+
+        logger.info("Extracting historical data from RDS...")
+        raw_carbon_data = get_historical_carbon_data(conn)
+        raw_generation_data = get_historical_power_generation_data(conn)
+        logger.info("Historical data extraction complete.")
+        conn.close()
+
+        logger.info("Preparing data for S3 upload...")
+        partitioned_carbon_data = prepare_carbon_data(raw_carbon_data)
+        partitioned_generation_data = prepare_power_cut_data(
+            raw_generation_data)
+        logger.info("Data preparation complete.")
+
+        logger.info("Uploading data to S3...")
+        upload_carbon_data_to_s3(partitioned_carbon_data)
+        upload_energy_generation_data_to_s3(partitioned_generation_data)
+        logger.info("Data upload to S3 complete.")
 
     except Exception as e:
         logger.error("An error occurred: %s", e)
@@ -33,7 +60,7 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': 'Power cut data successfully extracted from RDS and uploaded to S3.'
+        'body': 'Power generation data successfully extracted from RDS and uploaded to S3.'
     }
 
 
