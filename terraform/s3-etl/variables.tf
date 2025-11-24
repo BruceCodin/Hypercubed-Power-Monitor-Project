5b63a98d478c51f8b52fe9dbc0f1681c08540329
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-24 13:14:57 +0000

    feat: Add variables

--- terraform/s3-etl/variables.tf
+++ terraform/s3-etl/variables.tf
@@ -15,3 +15,41 @@ variable "ecr_repository_name" {
   type        = string
   default     = "c20-power-monitor-s3-etl-repo"
 }
+
+# Lambda Configuration
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "c20-power-monitor-powercuts-etl-lambda"
+}
+
+variable "lambda_timeout" {
+  description = "Lambda timeout in seconds"
+  type        = number
+  default     = 300  # 5 minutes
+}
+
+variable "lambda_memory_size" {
+  description = "Lambda memory size in MB"
+  type        = number
+  default     = 512
+}
+
+# Secrets Manager
+variable "secrets_manager_arn" {
+  description = "ARN of the existing Secrets Manager secret containing DB credentials and API keys"
+  type        = string
+}
+
+# EventBridge Scheduler Configuration
+variable "scheduler_enabled" {
+  description = "Enable or disable the EventBridge scheduler"
+  type        = bool
+  default     = true
+}
+
+variable "secrets_manager_arn" {
+  description = "ARN of the existing Secrets Manager secret containing DB credentials and API keys"
+  type        = string
+  default     = "arn:aws:secretsmanager:eu-west-2:129033205317:secret:c20-power-monitor-db-credentials-TAc5Xx"
+}
