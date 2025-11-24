75845cb3011ccf51bbe18e51669a89725332c145
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-24 20:13:00 +0000

    feat: Add bash scripts

---
+++ pipelines/s3_pipeline/power_generation/bash_scripts/push_s3_lambda_image.sh
@@ -0,0 +1,12 @@
+S3_ECR_URL="129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-power-monitor-s3-power-gen-etl-repo"
+S3_ECR_NAME="c20-power-monitor-s3-power-gen-etl-repo"
+AWS_REGION="eu-west-2"
+
+# 1. Authenticate Docker to ECR
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin $S3_ECR_URL
+
+# 2. Tag local image with the ECR repository URL
+docker tag $S3_ECR_NAME:latest $S3_ECR_URL:latest
+
+# 3. Push image to ECR
+docker push $S3_ECR_URL:latest
