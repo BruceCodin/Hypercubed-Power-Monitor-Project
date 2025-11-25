a192b4add852600cc00920cf9b06e50fa2480cb7
Bruce <bruceachen@gmail.com>
2025-11-25 17:00:06 +0000

    feat: ecr terraform file implemented

---
+++ terraform/dashboard/ecr.tf
@@ -0,0 +1,28 @@
+# ecr.tf - Elastic Container Registry for Dashboard
+
+# ECR Repository
+resource "aws_ecr_repository" "dashboard" {
+  name                 = var.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Outputs
+
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.dashboard.repository_url
+}
+
+output "ecr_repository_arn" {
+  description = "ARN of the ECR repository"
+  value       = aws_ecr_repository.dashboard.arn
+}
+
+output "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  value       = aws_ecr_repository.dashboard.name
+}
