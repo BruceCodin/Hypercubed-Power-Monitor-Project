7e6f72615695b79698bf684801f528063903c224
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-25 18:29:53 +0000

    fix: fix docker push script

--- pipelines/dashboard_pipeline/bash_scripts/push_image_to_ecr.bash
+++ pipelines/dashboard_pipeline/bash_scripts/push_image_to_ecr.bash
@@ -1,13 +1,22 @@
 
-DASHBOARD_ECR_URL="129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-power-monitor-dashboard-repo"
-DASHBOARD_ECR_NAME="c20-power-monitor-dashboard-repo"
+# ECR Registry URL
+ECR_REGISTRY="129033205317.dkr.ecr.eu-west-2.amazonaws.com"
+
+# ECR Repository name
+ECR_REPOSITORY="c20-power-monitor-dashboard-repo"
+
+# Full ECR image URI (registry + repository)
+ECR_IMAGE_URI="$ECR_REGISTRY/$ECR_REPOSITORY"
+
+# Local image name
+LOCAL_IMAGE_NAME="c20-power-monitor-alerts-dashboard"
 AWS_REGION="eu-west-2"
 
 # 1. Authenticate Docker to ECR
-aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin $DASHBOARD_ECR_URL
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ECR_REGISTRY"
 
-# 2. Tag local image with the ECR repository URL
-docker tag $DASHBOARD_ECR_NAME:latest $DASHBOARD_ECR_URL:latest
+# 2. Tag local image with the ECR image URI
+docker tag "$LOCAL_IMAGE_NAME:latest" "$ECR_IMAGE_URI:latest"
 
 # 3. Push image to ECR
-docker push $DASHBOARD_ECR_URL:latest
+docker push "$ECR_IMAGE_URI:latest"
