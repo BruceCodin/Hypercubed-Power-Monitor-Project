b362cdc5378bdb2c028185a27d9d6e3c81d0a5fe
Bruce <bruceachen@gmail.com>
2025-11-25 11:23:47 +0000

    fix: fixed variable mismatch name in db credentials

--- ai_summary/generate_ai_summary.py
+++ ai_summary/generate_ai_summary.py
@@ -58,7 +58,7 @@ def get_db_connection():
     conn = psycopg2.connect(
         host=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
-        user=os.environ['DB_USERNAME'],
+        user=os.environ['DB_USER'],
         password=os.environ['DB_PASSWORD'],
         port=os.environ.get('DB_PORT', '5432')
     )
