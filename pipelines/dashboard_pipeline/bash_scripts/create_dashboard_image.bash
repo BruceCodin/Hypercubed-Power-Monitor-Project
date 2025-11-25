9b701e1bd53d4cce81e7186acbe7561c7a53930b
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-25 17:49:57 +0000

    fix: Use newest ECR

--- pipelines/dashboard_pipeline/bash_scripts/create_dashboard_image.bash
+++ pipelines/dashboard_pipeline/bash_scripts/create_dashboard_image.bash
@@ -1,2 +1,2 @@
-docker build --platform linux/amd64 --no-cache -t c20-power-monitor-alerts-etl-repo:latest ../.
+docker build --platform linux/amd64 --no-cache -t c20-power-monitor-alerts-dashboard:latest ../.
 
--- pipelines/dashboard_pipeline/bash_scripts/push_image_to_ecr.bash
+++ pipelines/dashboard_pipeline/bash_scripts/push_image_to_ecr.bash
@@ -1,1 +1,13 @@
 
+DASHBOARD_ECR_URL="129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-power-monitor-dashboard-repo"
+DASHBOARD_ECR_NAME="c20-power-monitor-dashboard-repo"
+AWS_REGION="eu-west-2"
+
+# 1. Authenticate Docker to ECR
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin $DASHBOARD_ECR_URL
+
+# 2. Tag local image with the ECR repository URL
+docker tag $DASHBOARD_ECR_NAME:latest $DASHBOARD_ECR_URL:latest
+
+# 3. Push image to ECR
+docker push $DASHBOARD_ECR_URL:latest
