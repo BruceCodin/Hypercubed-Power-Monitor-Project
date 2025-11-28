7f410e52a1bdd57a4d02a125f58e44800f0949c3
CrunchBerryCaptain <mohammadmuarijb@yahoo.co.uk>
2025-11-28 11:05:17 +0000

    feat: add bash scripts

---
+++ pipelines/summaries_pipeline/bash_scripts/update_summaries_lambda_image.sh
@@ -0,0 +1,3 @@
+aws lambda update-function-code \
+    --function-name c20-power-monitor-lambda-alerts-pipeline  \
+    --image-uri 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c20-power-monitor-alerts-etl-repo:latest
